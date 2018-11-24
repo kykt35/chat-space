55b91a293f5c5fce72de49f4b384e58068271a00
kykt35 <kykt35@gmail.com>
2018-11-24 20:12:15 +0900

    Edit messages_controller to add create action

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,19 @@
 class MessagesController < ApplicationController
   def index
-
   end
+
+  def create
+    if Message.create(message_params)
+      redirect_to group_messages_path(@group)
+    else
+      redirect_to group_messages_path(@group), alert: "メセージを入力してください"
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :image)
+  end
+
 end
