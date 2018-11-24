b87fafe55af373bc5e2a7ae915742d622d366f35
kykt35 <kykt35@gmail.com>
2018-11-24 19:32:48 +0900

    Cange root path message#index to group#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'messages#index'
+  root 'groups#index'
   resources :users ,only: [:edit, :update]
   resources :groups, only: [:index, :create, :update, :edit, :new] do
     resources :messages, only: [:index, :create]
