e1164bd17f7021fc48048cf20c8ebf5e4ac35ef8
kykt35 <kykt35@gmail.com>
2018-11-24 17:40:45 +0900

    Edit group create view

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,14 +1,26 @@
 class GroupsController < ApplicationController
 
   def new
+    @group = Group.new
   end
 
   def edit
   end
 
   def create
+    if Group.create(groups_params)
+      redirect_to root_path, notice: "グループを作成しました"
+    else
+      render :new
+    end
   end
 
   def update
+  end
+
+  private
+
+  def groups_params
+    params.require(:group).permit(:name, :member)
   end
 end
