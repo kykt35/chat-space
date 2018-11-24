0e9d80c223e9eaa04779f89530e176f9fe1a7917
kykt35 <kykt35@gmail.com>
2018-11-24 20:44:01 +0900

    Edit message controller

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,19 +1,30 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+
   def index
+    @message = Message.new
+    @messeges = @group.messages.includes(:user)
   end
 
   def create
-    if Message.create(message_params)
-      redirect_to group_messages_path(@group)
+    @message = @group.message.new(message_params)
+    if @message.save
+      redirect_to group_messages_path(@group), notice: "メッセージが送信されました"
     else
-      redirect_to group_messages_path(@group), alert: "メセージを入力してください"
+      #@messeges = @group.messages.include(:user)
+      flash.now[:alert] = "メセージを入力してください"
+      render :index
+
     end
   end
 
   private
 
   def message_params
-    params.require(:message).permit(:body, :image)
+    params.require(:message).permit(:body, :image).merge(user_id: current_user.id)
   end
 
+  def set_group
+    @group = Group.find(params[:group_id])
+  end
 end
