7161985344508536f66721348dc96dec0be96ccd
kykt35 <kykt35@gmail.com>
2018-11-26 20:00:42 +0900

    Add comment in message.rb

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,6 +5,7 @@ class Message < ApplicationRecord
   validates :content,presence: true, unless: :image?
   mount_uploader :image, ImagesUploader
 
+  #作成日の表示形式を整形
   def strp_created_at
     self.created_at.in_time_zone('Asia/Tokyo').strftime('%Y/%m/%d %H:%M:%S')
   end
