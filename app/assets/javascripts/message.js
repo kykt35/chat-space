d6b8311f5484687a19ecc2feb41df502956940ea
kykt35 <kykt35@gmail.com>
2018-11-26 17:34:23 +0900

    Edit message.js to add ajax method

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -2,5 +2,21 @@ $(function(){
   $('#new_message').on('submit', function(e){
     e.preventDefault();
     console.log("submit");
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(message){
+      console.log("done");
+    })
+    .fail(function(){
+      alert('error');
+    })
   });
 })
