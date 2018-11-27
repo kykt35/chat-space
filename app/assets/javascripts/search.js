29071ed8388fbaeec4b299861cf03c682078c471
kykt35 <kykt35@gmail.com>
2018-11-27 17:17:19 +0900

    Edit search.js to add group user append and remove search result

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -8,10 +8,23 @@ $(function(){
             </div>`
     $('#user-search-result').append(html);
   }
+  function appendGroupUser(user){
+    html = `<div class='chat-group-user clearfix js-chat-member' id='chat-group-user-${user.id}'>
+      <input name='group[user_ids][]' type='hidden' value=${user.id}>
+      <p class='chat-group-user__name'>${user.name}</p>
+      <a class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</a>
+    </div>`
+    $('#chat-group-users').append(html);
+    console.log("append group user:"+user.id);
+  }
 
   $('#user-search-result').on("click",".user-search-add",function(){
-    var id =  $(this).attr("data-user-id");
-    console.log(id);
+    var user = {id: $(this).attr("data-user-id"), name: $(this).attr("data-user-name")};
+    $(this).parent().remove();
+    appendGroupUser(user);
+  });
+  $('#chat-group-users').on("click",".user-search-remove",function(){
+    $(this).parent().remove();
   });
 
   $('#user-search-field').on("keyup", function(){
