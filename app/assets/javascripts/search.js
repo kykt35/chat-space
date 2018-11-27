f3f2f440f5b7b4a823041d4cfbdef2ae06e819f6
kykt35 <kykt35@gmail.com>
2018-11-27 14:34:07 +0900

    Edit to append search result html

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,18 +1,31 @@
 $(function(){
+  var $search_result = $('#user-search-result')
+  function appendSearchUser(user){
+    html = `<div class="chat-group-user clearfix">
+              <p class="chat-group-user__name">${user.name}</p>
+              <a class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id=${user.id} data-user-name=${user.name}>追加</a>
+            </div>`
+    $('#user-search-result').append(html);
+  }
+
   $('#user-search-field').on("keyup", function(){
     var input = $('#user-search-field').val();
-    console.log(input);
     $.ajax({
       type: "GET",
       url:  '/users',
       data: {keyword: input},
       dataType: 'json'
     })
-    .done(function(data){
-      console.log("done");
+    .done(function(users){
+      $search_result.empty();
+      if (users.length !== 0){
+        users.forEach(function(user){
+          appendSearchUser(user);
+        });
+      }
     })
     .fail(function(){
-      console.log("fail");
+      alert("ユーザー検索に失敗しました");
     })
   })
 });
