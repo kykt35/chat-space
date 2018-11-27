d14b8bc1da10f9e21bd846c9f3cb53434af6df89
kykt35 <kykt35@gmail.com>
2018-11-27 19:21:22 +0900

    Edit users_controller do not search current user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
 
   def index
-    @users = User.where('name LIKE(?)', "%#{params[:keyword]}%")
+    @users = User.where.not(id: current_user.id).where('name LIKE(?)', "%#{params[:keyword]}%")
   end
 
   private
