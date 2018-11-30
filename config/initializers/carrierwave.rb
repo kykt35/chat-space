39bfb09543afedecf75046b1b8613330fd5ede2d
kykt35 <kykt35@gmail.com>
2018-12-01 03:21:39 +0900

    Edit carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -11,7 +11,6 @@ CarrierWave.configure do |config|
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-
   config.fog_directory  = 'chat-space-upload-kykt35'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/chat-space-upload-kykt35'
 end
