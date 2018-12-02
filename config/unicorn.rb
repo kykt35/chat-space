cda7016fb246f529066559019932286ce61ee2b5
kykt35 <kykt35@gmail.com>
2018-12-02 15:47:56 +0900

    Fix unicorn.rb to comment out listen 3000

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -8,7 +8,7 @@ listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
 stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-listen 3000
+#listen 3000
 timeout 60
 
 preload_app true
